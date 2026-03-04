46e04b1bc1405626b41761a396ad62cd6a3d20b4
Plonoton <konsti391@gmail.com>
2026-03-05 00:04:27 +0100

    Fix offline support - try #2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760229769" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "458786011" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
